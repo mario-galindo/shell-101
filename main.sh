83d49ebeb3acc53a471ff1623ac69ddf6b6bd20e
mario-galindo <mgalindo9410@gmail.com>
2022-06-21 19:25:15 -0600

    conditional resource group creation

--- main.sh
+++ main.sh
@@ -1,18 +1,19 @@
 echo Installing required Azure CLI extensions - this may take a few minutes...
 
 rg=$(az group list --query "[].name" -o tsv);
-if [[ "$rg" != *learn* ]]; then
+if [[ "$rg" != *learn* ]]; 
+then
     rgguid=$(uuidgen)
     rgsuffix=${rgguid//[-]/}
     rgsuffix=${rgsuffix:0:18}
     rg=learn${rgsuffix}
     az group create --name $rg --location eastus --output none
+else
+    rglist=$rg
+    searchstring="learn"
+    rest=${rglist#*$searchstring}
+    index=$(( ${#rglist} - ${#rest} - ${#searchstring} ))
+    rg=${rglist:index:23}
 fi
 
-
-rglist=$rg
-searchstring="learn"
-
-rest=${rglist#*$searchstring}
-index=$(( ${#rglist} - ${#rest} - ${#searchstring} ))
-echo "Using the ${rglist:index:23} resource group..."
+echo "Using the $rg resource group..."
